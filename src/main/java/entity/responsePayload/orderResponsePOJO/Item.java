77f0c6424835dbd3ec0418fc6bbd716bed6d9168
Harsh-Patel-7 <58640315+harshvegada@users.noreply.github.com>
2026-07-16 09:20:16 +0530

    Delete src directory

--- src/main/java/entity/responsePayload/orderResponsePOJO/Item.java
+++
@@ -1,9 +0,0 @@
-package entity.responsePayload.orderResponsePOJO;
-
-public class Item {
-    public String itemId;
-    public String itemName;
-    public String category;
-    public Integer quantity;
-    public Integer price;
-}
